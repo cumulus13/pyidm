4ff9590580f2bbadb8a9c1e5c39703883c46d528
tester001 <todut001@gmail.com>
2019-04-24 18:34:54 +0700

    version: 0.33 ~ 2019-04-24 18:34:46:694000

--- idm/__init__.py
+++ idm/__init__.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python2
 
+import imp
+version = imp.load_source('version', "../__version__.py")
+__version__ 	= version.version
+__email__		= "licface@yahoo.com"
+__author__		= "licface@yahoo.com"
+
 from idm import *
