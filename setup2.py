70fecc39dddb3f5f3a6d49e1a8989fd9ba14b1aa
yams <licface13@gmail.com>
2020-10-30 19:26:40 +0700

    version: 0.40 ~ 2020-10-30 19:26:20:561006

---
+++ setup2.py
@@ -0,0 +1,75 @@
+import io
+import re
+from setuptools import setup
+
+import os
+import shutil
+
+#  try:
+    #  shutil.rmtree(os.path.join(os.path.dirname(__file__), 'build'))
+#  except:
+    #  pass
+#  try:
+    #  shutil.rmtree(os.path.join(os.path.dirname(__file__), 'dist'))
+#  except:
+    #  pass
+try:
+    os.makedirs(os.path.join(os.path.dirname(__file__), 'pyidm'))
+except:
+    pass
+shutil.copy2(os.path.join(os.path.dirname(__file__), 'idm', 'idm.py'), 'pyidm')
+shutil.copy2(os.path.join(os.path.dirname(__file__), 'idm', '__main__.py'), 'pyidm')
+shutil.copy2(os.path.join(os.path.dirname(__file__), 'idm', '__version__.py'), 'pyidm')
+shutil.copy2(os.path.join(os.path.dirname(__file__), 'idm', '__init__.py'), 'pyidm')
+
+with io.open("README.rst", "rt", encoding="utf8") as f:
+    readme = f.read()
+
+import __version__
+version = __version__.version
+
+setup(
+    name="pyidm",
+    version=version,
+    url="https://bitbucket.org/licface/idm",
+    project_urls={
+        "Documentation": "https://bitbucket.org/licface/idm",
+        "Code": "https://bitbucket.org/licface/idm",
+    },
+    license="BSD",
+    author="Hadi Cahyadi LD",
+    author_email="cumulus13@gmail.com",
+    maintainer="cumulus13 Team",
+    maintainer_email="cumulus13@gmail.com",
+    description="Downloader with Internet Download Manager (Windows)",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=["pyidm"],
+    install_requires=[
+        'argparse',
+        'pypiwin32',
+        'comtypes'
+    ],
+    entry_points = {
+         "console_scripts": [
+             "pyidm = pyidm.__main__:usage",
+         ]
+    },
+    data_files=['__version__.py', 'README.rst', 'LICENSE.rst'],
+    include_package_data=True,
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
